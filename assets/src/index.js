a09f07a486243d4a13ca3ae7182fd98a358009a0
riley-ad-clark <rileyclarky@gmail.com>
2023-11-08 11:13:43 -0600

    Added function for seeing if the number is negative or positive, in return changing the color to red or green respectively

--- assets/src/index.js
+++ assets/src/index.js
@@ -15,46 +15,69 @@ const numberElement = document.getElementById('number');
 
 numberElement.innerText = '0';
 
+/*--------number color change--------*/
+
+function colorCheck () {
+    let number = parseInt(numberElement.innerText, 10)
+    if (number > 0) {
+        numberElement.style.color = '#32e33e'
+    } else if (number < 0) {
+        numberElement.style.color = '#eb3535'
+    } else {
+        numberElement.style.color = '#000000'
+    }
+}
+
+/*--------end of number color change--------*/
+
 /*--------buttons--------*/
 
 decreaseOne.addEventListener('click', () => {
     let number = parseInt(numberElement.innerText, 10);
     number--;
     numberElement.innerText = number.toString();
+    colorCheck();
 });
 
 decreaseFive.addEventListener('click', () => {
     let number = parseInt(numberElement.innerText, 10);
     number -= 5;
     numberElement.innerText = number.toString();
+    colorCheck();
 });
 
 decreaseTen.addEventListener('click', () => {
     let number = parseInt(numberElement.innerText, 10);
     number -= 10;
     numberElement.innerText = number.toString();
+    colorCheck();
 });
 
 increaseOne.addEventListener('click', () => {
     let number = parseInt(numberElement.innerText, 10);
     number++;
     numberElement.innerText = number.toString();
+    colorCheck();
 });
 
 increaseFive.addEventListener('click', () => {
     let number = parseInt(numberElement.innerText, 10);
     number += 5;
     numberElement.innerText = number.toString();
+    colorCheck();
 });
 
 increaseTen.addEventListener('click', () => {
     let number = parseInt(numberElement.innerText, 10);
     number += 10;
     numberElement.innerText = number.toString();
+    colorCheck();
 });
 
 reset.addEventListener('click', () => {
     number.innerText = '0'
+    colorCheck();
 })
 
 /*--------end of buttons--------*/
+
